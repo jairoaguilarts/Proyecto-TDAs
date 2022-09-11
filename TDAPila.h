e19ed313fbb7b49bd9277428bcbba4d8af9750d5
jairoaguilarts <jairoaguilar1219@gmail.com>
2022-09-10 22:13:52 -0600

    Headers

---
+++ TDAPila.h
@@ -0,0 +1,16 @@
+#ifndef TDAPILA_H
+#define TDAPILA_H
+
+#include "Object.h"
+
+class TDAPila {
+	public:
+		virtual void anula() = 0;
+		virtual Object* tope() = 0;
+		virtual void mete(Object*) = 0;//push	
+		virtual Object* saca() = 0;//pop
+		virtual bool vacia() = 0;
+		virtual void imprime_pila() = 0;
+};
+
+#endif
