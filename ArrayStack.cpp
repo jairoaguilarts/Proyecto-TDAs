8e611a1695ad8c1201331ecc386ba0f66c91c044
jairoaguilarts <jairoaguilar1219@gmail.com>
2022-09-12 14:23:51 -0600

    Implementaciones stacks

--- ArrayStack.cpp
+++ ArrayStack.cpp
@@ -1,20 +1,20 @@
 #include "ArrayStack.h"
+#include <iostream>
+using namespace std;
 
 #define NULL 0
 
-ArrayStack::ArrayStack()
-{
-	n=0;
+ArrayStack::ArrayStack(){
+	n = 0;
 	capacidad = 10;
 	array = new Object*[capacidad];
-	for(int i=0; i<capacidad;i++)
+	for(int i = 0; i < capacidad; i++)
 		array[i] = NULL;
 }
 
-ArrayStack::~ArrayStack()
-{
-		//libera el contenido de las casillas
-	for(int i=0; i<capacidad; i++){
+ArrayStack::~ArrayStack(){
+	//libera el contenido de las casillas
+	for(int i = 0; i<capacidad; i++){
 		if(array[i]){
 			delete array[i];
 		}
@@ -22,3 +22,59 @@ ArrayStack::~ArrayStack()
 	//libera el arreglo
 	delete[] array;
 } 
+
+void ArrayStack::anula(){
+	for (int i = n - 1; i >= 0; i--){
+		array[i] = NULL;
+	}
+	n = 0;
+}
+
+Object* ArrayStack::tope(){
+	return array[n - 1];
+}
+
+void ArrayStack::mete(Object* item){
+	if(n == capacidad){
+		capacidad += 15;
+		Object** temp = new Object*[capacidad];
+		for (int i = 0; i < n; i++) {
+			temp[i] = array[i];
+		}
+		delete[] array;
+		array = temp;
+	}
+	if(item != NULL){
+		array[n - 1] = item;
+		n++;
+	}
+	else
+		cout << "var tipo Object nulo";
+}
+
+Object* ArrayStack::saca(){
+	if(n != 0){
+		Object* temp = array[n - 1];
+		delete array[n - 1];
+		n--;
+		return temp;
+	}
+	cout << "La Pila esta vacia";
+	return NULL;
+}
+
+bool ArrayStack::vacia(){
+	return n == 0;
+}
+
+void ArrayStack::imprime_pila(){
+	if(n == 0)
+		cout << "La Pila esta vacia";
+	else{
+		for (int i = 0; i < n; i++){
+			cout << endl;
+			cout << "-> " << array[i]->toString(); 
+		}
+		cout << endl;
+	}
+}
--- LinkedStack.cpp
+++ LinkedStack.cpp
@@ -1,4 +1,6 @@
 #include "LinkedStack.h"
+#include <iostream>
+using namespace std;
 
 LinkedStack::LinkedStack() {
 	inicio = nullptr;
@@ -8,3 +10,55 @@ LinkedStack::~LinkedStack() {
 	if(inicio)
 		delete inicio;
 } 
+
+void LinkedStack::anula(){
+	delete inicio;
+}
+
+Object* LinkedStack::tope(){
+	return inicio->getItem();
+}
+
+void LinkedStack::mete(Object* item){
+	if(inicio == NULL){
+		inicio->setItem(item);
+		inicio->setAnterior(NULL);
+	}
+	else{
+		Nodo* temp = new Nodo;
+		temp = inicio;
+		inicio->setItem(item);
+		temp->setAnterior(inicio->getAnterior());
+		inicio->setAnterior(temp);
+		temp->setSiguiente(inicio);
+	}
+}
+
+Object* LinkedStack::saca(){
+	if(inicio != NULL){
+		Nodo* temp = inicio;
+		Object* tempItem = inicio->getItem();
+		inicio = inicio->getAnterior();
+		temp = NULL;
+		delete temp;
+		return tempItem;
+	}
+	cout << "La Pila esta vacia!";
+	return NULL;
+}
+
+bool LinkedStack::vacia(){
+	return inicio == NULL;
+}
+
+void LinkedStack::imprime_pila(){
+	if(inicio != NULL){
+		Nodo* temp = inicio;
+		while(temp){
+			cout << temp->getItem()->toString() << " ";
+			temp = temp->getAnterior();
+		}
+	}
+	else
+		cout << "La Pila esta vacia";
+}
